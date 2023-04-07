653ba8627a2379dbd61700ff0f26ef7d2f41184d
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-06 20:03:48 -0500

    imported redux and react. made template object for sending customer info to reducer

--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -1,11 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from 'react';
 
 
 function Customer() {
 
     return (
         <>
+            <h1>Step 2: Enter Delivery Address and Choose Delivery Type</h1>
+            {/* Do I want to do a form or input? */}
+           
         </>
-    )
+    );
 }
 
-export default Customer
+export default Customer;
+
+/*
+
+Template Object for Reducer
+
+let customerInfo = {
+    name: '',
+    address: '',
+    city: '',
+    zip: '',
+    deliveryType: ''
+}
+
+
+*/
