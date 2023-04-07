3a39f48c441cb09e5002c8e5b8e478c664c87eb7
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-04-06 20:00:35 -0400

    imported redux logic and setup a store. also wrapped app with Provider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+import { createStore, combineReducers, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+
+
+//REDUCERS
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            // reducers go here
+        }
+    ),
+    applyMiddleware(logger)
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    
     <React.StrictMode>
+        <Provider store={storeInstance}>
         <App />
+        </Provider>
     </React.StrictMode>
 );
+
+
