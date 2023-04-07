6176d8dcee9289a59d86d5e5386139bfacee821f
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-04-06 21:05:52 -0400

    started work on checkout page

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,10 +1,20 @@
-
+import { useSelector, useDispatch } from "react-redux"
 
 function Checkout() {
+    const dispatch = useDispatch();
+
+    const info = useSelector(store => store.customerInfo);
+   
 
     return (
-        <>
-        </>
+        <div id="checkout">
+            <h2>Step 3: Checkout</h2>
+            <div id="customer-info">
+                <p>{info.name}</p>
+                <p>{info.street}</p>
+                <p>{info.city}, {info.state} {info.zip}</p>
+            </div>
+        </div>
     )
 }
 
