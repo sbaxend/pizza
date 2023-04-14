1df990a613d059b5612749a3b4bd89c3dac321aa
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-13 19:19:54 -0500

    using state to store form and send to redux onsubmit, got state working in placeholders for form

--- src/components/Customer/Customer.jsx
+++ src/components/Customer/Customer.jsx
@@ -1,14 +1,23 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function Customer() {
     const dispatch = useDispatch();
+    let [customerInfoState, setCustomerInfoState] = useState({ 
+        name: 'John Dingle',
+        address: '666 No Way',
+        city: 'Rack City',
+        zip: '12345',
+    });
 
-    const {name, address, city, zip } = useSelector(store => store.customerInfo);
+    // const { name, address, city, zip } = useSelector(store => store.customerInfo);
 
     const handleNameChange = (event) => {
-        //
+        const action = {
+            type: ''
+        }
     }
 
     const handleStreetChange = (event) => {
@@ -28,13 +37,13 @@ function Customer() {
             <h1>Step 2: Enter Delivery Address and Choose Delivery Type</h1>
             <form>
                 <label htmlFor="name">Name:</label><br />
-                <input id="name" type="text" onChange={handleNameChange} placeholder={name} /><br />
+                <input id="name" type="text" onChange={handleNameChange} placeholder={customerInfoState.name} /><br />
                 <label htmlFor="street">Street Address:</label><br />
-                <input id="street" type="text" onChange={handleStreetChange} placeholder={address}/><br />
+                <input id="street" type="text" onChange={handleStreetChange} placeholder={customerInfoState.address}/><br />
                 <label htmlFor="city">City:</label><br />
-                <input id="city" type="text" onChange={handleCityChange} placeholder={city} /><br />
+                <input id="city" type="text" onChange={handleCityChange} placeholder={customerInfoState.city} /><br />
                 <label htmlFor="zip">Zip Code:</label><br />
-                <input id="zip" type="text" onChange={handleZipChange} placeholder={zip} /><br />
+                <input id="zip" type="text" onChange={handleZipChange} placeholder={customerInfoState.zip} /><br />
                 <input type="radio" id="delivery" name="delivery_type" value="Delivery" />
                 <label htmlFor="delivery">Delivery</label><br />
                 <input type="radio" id="pickup" name="delivery_type" value="Pickup" />
