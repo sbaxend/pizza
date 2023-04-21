f4c95dc2e384cf59ff07f6c15fbe6be5e516d3e4
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-20 19:30:11 -0500

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
-
+import { useSelector } from 'react-redux';
 
 function Header() {
+    const totalCost = useSelector(store => store.totalCost);
+    const customersPizza = useSelector(store => store.customersPizza);
+
 
 
     return (
         <>
+            <h1>
+                {customersPizza.length === 1 ? 'This pizza is yours for the low price of:' :
+                                    'These pizzas are yours for the low price of:'} {totalCost}
+            </h1>
         </>
     )
 }
