a4aa9f87c50d7aca773eebc5db2b41a7391fd3e9
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-20 19:36:06 -0500

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,8 @@
-import { useSelector } from 'react-redux';import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function Header() {
     const totalCost = useSelector(store => store.totalCost);
     const customersPizza = useSelector(store => store.customersPizza);
-
-    const totalCost = useSelector(store => store.totalCost);
-    const customersPizza = useSelector(store => store.customersPizza);
-
-    // const costText = () => {
-    //     customersPizza.length === 1 ? 'This pizza is yours for the low price of:' :
-    //                                 'These pizzas are yours for the low price of:'
-    // }
 
     return (
         <>
